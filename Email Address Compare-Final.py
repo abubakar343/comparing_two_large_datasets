6750d30cd71b684d8e15b293fbfa3abd3f2ee75f
syed_abubakr <100688909+abubakar343@users.noreply.github.com>
2024-08-19 17:50:35 +0500

    Update Email Address Compare-Final.py

--- Email Address Compare-Final.py
+++ Email Address Compare-Final.py
@@ -4,7 +4,7 @@
 # In[4]:
 
 
-#This code convertes SG file to CSV 
+#This code converts Excel file to CSV 
 
 
 import pandas as pd
@@ -12,10 +12,10 @@ import os
 
 
 # Load the Excel file
-excel_file_path = 'SG All Emails dupes removed.xlsx'  # Replace with your Excel file path
+excel_file_path = 'first_excel_file.xlsx'  # Replace with your Excel file path
 
 # Define the output CSV file path
-csv_file_path = 'SG All Emails dupes removed.csv'  # Change this to your desired output file path
+csv_file_path = 'desired_name.csv'  # Change this to your desired output file path
 
 # Check if the CSV file already exists; if not, convert the Excel file to CSV
 if not os.path.isfile(csv_file_path):
@@ -28,15 +28,12 @@ if not os.path.isfile(csv_file_path):
     combined_df.to_csv(csv_file_path, index=False)
 
 
-# In[ ]:
-
-
-# This code reads SG CSV file, finds duplicates and creates a new CSV file.
+# This code reads newly-created CSV file, finds duplicates and creates a new CSV file.
 
 import pandas as pd
 import os
 
-df = pd.read_csv('SG All Emails dupes removed.csv',dtype={'Email Address': str},low_memory=False)
+df = pd.read_csv('second_file.csv',dtype={'Email Address': str},low_memory=False)
 
 # Create an empty list to store rows with duplicate emails
 duplicate_email_rows = []
@@ -52,12 +49,8 @@ duplicate_email_df = pd.DataFrame(duplicate_email_rows)
 duplicate_email_df.to_csv('duplicate_emails_in_same_file.csv', index=False)
 
 
-# In[ ]:
+#This code converts second Excel file to CSV 
 
-
-#This code convertes master file to CSV 
-
-import pandas as pd
 
 # Load the Excel file
 excel_file_path = 'master_email_sheet.xlsx'  # Replace with the path to your Excel file
@@ -87,15 +80,15 @@ combined_df_master.to_csv(csv_file_path, index=False)
 # In[ ]:
 
 
-#This code reads 2 CSV files; master and SG and then compares the email addresses and then creates a new file.
+#This code reads 2 CSV files and then compares the email addresses and then creates a new file.
 
 
 import pandas as pd
 import os
 
 
-sg_emails_df = pd.read_csv('SG All Emails dupes removed.csv',dtype={'Email Address': str},low_memory=False)
-master_emails_df = pd.read_csv('master_email_sheet.csv',dtype={'Email': str},low_memory=False)
+sg_emails_df = pd.read_csv('first_file.csv',dtype={'Email Address': str},low_memory=False)
+master_emails_df = pd.read_csv('second_file.csv',dtype={'Email': str},low_memory=False)
 
 # Extract the 'Email Address' column from each DataFrame
 sg_emails = sg_emails_df['Email Address']
@@ -110,51 +103,11 @@ common_emails_df = pd.DataFrame({'Common Email': common_emails})
 common_emails_df.to_csv('duplicate_emails_in_master_file.csv', index=False)
 
 
-# In[18]:
-
-
-# Check if 'lee@danbinford.com' exists in the 'Email' column of master file
-email_to_check = 'info@muherlaw.com'
+# Check if 'abc@example.com' exists in the 'Email' column of second file
+email_to_check = 'abc@example.com'
 is_present=  master_emails_df['Email'].isin([email_to_check]).any()
 
 if is_present:
     print(f"{email_to_check} exists in master file.")
 else:
     print(f"{email_to_check} does not exist in master file.")
-
-
-# In[19]:
-
-
-# Check if 'lee@danbinford.com' exists in the 'Email Address' column of SG file
-email_to_check = 'info@muherlaw.com'
-is_present = sg_emails_df['Email Address'].isin([email_to_check]).any()
-
-if is_present:
-    print(f"{email_to_check} exists in SG file.")
-else:
-    print(f"{email_to_check} does not exist in SF file.")
-
-
-# In[10]:
-
-
-
-import pandas as pd
-import os
-
-
-master_emails_df = pd.read_csv('test Master Bounce List 20231008.csv',dtype={'Email Address': str},low_memory=False,encoding="ISO-8859-1")
-
-
-# In[11]:
-
-
-master_emails_df
-
-
-# In[ ]:
-
-
-
-
