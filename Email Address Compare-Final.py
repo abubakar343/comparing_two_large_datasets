9483160497e828b40f52d7adb8fd43cb85e5fcaa
syed_abubakr <100688909+abubakar343@users.noreply.github.com>
2024-08-19 17:53:21 +0500

    Update Email Address Compare-Final.py

--- Email Address Compare-Final.py
+++ Email Address Compare-Final.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
-#This code converts Excel file to CSV 
-
-
+# import libraries
 import pandas as pd
 import os
 
-
-# Load the Excel file
+#This code converts Excel file to CSV 
 excel_file_path = 'first_excel_file.xlsx'  # Replace with your Excel file path
-
-# Define the output CSV file path
 csv_file_path = 'desired_name.csv'  # Change this to your desired output file path
 
 # Check if the CSV file already exists; if not, convert the Excel file to CSV
@@ -27,12 +19,7 @@ if not os.path.isfile(csv_file_path):
     # Save the combined DataFrame as a CSV file
     combined_df.to_csv(csv_file_path, index=False)
 
-
 # This code reads newly-created CSV file, finds duplicates and creates a new CSV file.
-
-import pandas as pd
-import os
-
 df = pd.read_csv('second_file.csv',dtype={'Email Address': str},low_memory=False)
 
 # Create an empty list to store rows with duplicate emails
@@ -43,20 +30,15 @@ duplicates = df[df['Email Address'].duplicated()]
 
 # Append entire rows with duplicate emails to the list
 duplicate_email_rows.extend(duplicates.to_dict('records'))
-
 duplicate_email_df = pd.DataFrame(duplicate_email_rows)
-
 duplicate_email_df.to_csv('duplicate_emails_in_same_file.csv', index=False)
 
 
 #This code converts second Excel file to CSV 
-
-
-# Load the Excel file
-excel_file_path = 'master_email_sheet.xlsx'  # Replace with the path to your Excel file
+excel_file_path = 'second_file.xlsx'  # Replace with the path to your Excel file
 
 # Define the output CSV file path
-csv_file_path = 'master_email_sheet.csv'  # Change this to your desired output file path
+csv_file_path = 'second_file.csv'  # Change this to your desired output file path
 
 # Initialize an empty list to store DataFrames
 all_dataframes = []
@@ -77,18 +59,12 @@ combined_df_master = pd.concat(all_dataframes, ignore_index=True)
 combined_df_master.to_csv(csv_file_path, index=False)
 
 
-# In[ ]:
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #This code reads 2 CSV files and then compares the email addresses and then creates a new file.
 
-
-import pandas as pd
-import os
-
-
-sg_emails_df = pd.read_csv('first_file.csv',dtype={'Email Address': str},low_memory=False)
-master_emails_df = pd.read_csv('second_file.csv',dtype={'Email': str},low_memory=False)
+sg_emails_df = pd.read_csv('first_file.csv',dtype={'Email Address': str},low_memory=False) #change the variable name as per your file
+master_emails_df = pd.read_csv('second_file.csv',dtype={'Email': str},low_memory=False) #change the variable name as per your file
 
 # Extract the 'Email Address' column from each DataFrame
 sg_emails = sg_emails_df['Email Address']
@@ -99,9 +75,7 @@ common_emails = sg_emails[sg_emails.isin(master_emails)]
 
 # Create a DataFrame with the common email addresses
 common_emails_df = pd.DataFrame({'Common Email': common_emails})
-
 common_emails_df.to_csv('duplicate_emails_in_master_file.csv', index=False)
-
 
 # Check if 'abc@example.com' exists in the 'Email' column of second file
 email_to_check = 'abc@example.com'
